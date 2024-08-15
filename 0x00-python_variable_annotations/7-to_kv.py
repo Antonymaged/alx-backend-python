2a60132171998a87be816fe761093f23f0b7fef3
Antonymaged <magedantony1@gmail.com>
2024-08-15 02:31:40 +0000

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""this module take a string and a number and return a tuple"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple(str, float):
+    """this function take a string and a number and return a tuple"""
+    return (k, v ** 2)
