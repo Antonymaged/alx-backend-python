e987a8263396079d8cf646ae78614bf2af79cc48
Antonymaged <magedantony1@gmail.com>
2024-08-15 02:01:41 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" this module have four variables a, pi, i_understand_annotations, school"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
