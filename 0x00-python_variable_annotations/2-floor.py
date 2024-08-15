981e00dc2bc9e8bc1d0f0e0635c061b06ef36b6d
Antonymaged <magedantony1@gmail.com>
2024-08-15 01:40:09 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+This module provides a function to calculate the floor of a float number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float number.
+
+    Args:
+        n (float): The float number to be floored.
+
+    Returns:
+        int: The largest integer less than or equal to `n`.
+
+    Example:
+        >>> floor(4.7)
+        4
+    """
+    return math.floor(n)
