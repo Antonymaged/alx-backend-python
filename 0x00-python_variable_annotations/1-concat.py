f40766c59ec0b6af2c70f4fe4ee1cf0b289b9037
Antonymaged <magedantony1@gmail.com>
2024-08-15 01:36:01 +0000

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+"""
+This module provides a simple function to concatenate two strings.
+"""
+
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The concatenated result of `str1` and `str2`.
+
+    Example:
+        >>> concat("Hello, ", "World!")
+        'Hello, World!'
+    """
     return str1 + str2
